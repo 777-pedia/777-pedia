aeca24dd12abe95acf3351cf41a992af20db51ea
seonrizee <seonrizee@gmail.com>
2025-09-26 08:59:32 +0900

    refactor: SearchHistory 필드명 오타 수정

--- src/main/java/org/example/pedia_777/domain/searchhistory/entity/SearchHistory.java
+++ src/main/java/org/example/pedia_777/domain/searchhistory/entity/SearchHistory.java
@@ -1,21 +1,19 @@
 package org.example.pedia_777.domain.searchhistory.entity;
 
-import java.time.LocalDateTime;
-
-import org.example.pedia_777.common.entity.BaseTimeEntity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import org.example.pedia_777.common.entity.BaseTimeEntity;
 
 @Entity
 public class SearchHistory extends BaseTimeEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private String keyword;
-	private LocalDateTime searchdAt;
+    private String keyword;
+    private LocalDateTime searchedAt;
 }
