db8c522bed88d58e89125d6eb441790723ddf217
seonrizee <seonrizee@gmail.com>
2025-09-25 18:35:39 +0900

    feat: Movies 엔티티에 Builder 패턴 및 정적 팩토리 메서드 추가

--- src/main/java/org/example/pedia_777/domain/movie/entity/Movies.java
+++ src/main/java/org/example/pedia_777/domain/movie/entity/Movies.java
@@ -1,35 +1,68 @@
 package org.example.pedia_777.domain.movie.entity;
 
-import java.time.LocalDateTime;
-
-import org.example.pedia_777.common.entity.BaseTimeEntity;
-import org.springframework.data.annotation.LastModifiedDate;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.pedia_777.common.entity.BaseTimeEntity;
+import org.springframework.data.annotation.LastModifiedDate;
 
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @Entity
 public class Movies extends BaseTimeEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private String title;
-	private String director;
-	private String actors;
-	private String genres;
-	private LocalDateTime releaseDate;
-	private Long runtime;
-	private String country;
-	private String overview;
-	private String posterUrl;
+    private String title;
+    private String director;
+    private String actors;
+    private String genres;
+    private LocalDateTime releaseDate;
+    private Long runtime;
+    private String country;
+    private String overview;
+    private String posterUrl;
 
-	@LastModifiedDate
-	@Temporal(TemporalType.TIMESTAMP)
-	private LocalDateTime updatedAt;
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Movies(String director, String title, String actors, String genres, LocalDateTime releaseDate, Long runtime,
+                  String country, String overview, String posterUrl) {
+        this.director = director;
+        this.title = title;
+        this.actors = actors;
+        this.genres = genres;
+        this.releaseDate = releaseDate;
+        this.runtime = runtime;
+        this.country = country;
+        this.overview = overview;
+        this.posterUrl = posterUrl;
+    }
+
+    public static Movies of(String director, String title, String actors, String genres, LocalDateTime releaseDate,
+                            Long runtime,
+                            String country, String overview, String posterUrl) {
+        return Movies.builder()
+                .director(director)
+                .title(title)
+                .actors(actors)
+                .genres(genres)
+                .releaseDate(releaseDate)
+                .runtime(runtime)
+                .country(country)
+                .overview(overview)
+                .posterUrl(posterUrl)
+                .build();
+    }
 }
