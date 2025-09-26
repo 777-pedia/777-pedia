6956cfae3ae6d4bb3d84ad45853634e809d70c28
soo59599 <sooh59599@gmail.com>
2025-09-26 14:17:51 +0900

    feat: 좋아요 취소 구현

--- src/main/java/org/example/pedia_777/domain/like/repository/LikeRepository.java
+++ src/main/java/org/example/pedia_777/domain/like/repository/LikeRepository.java
@@ -1,10 +1,13 @@
 package org.example.pedia_777.domain.like.repository;
 
+import java.util.Optional;
 import org.example.pedia_777.domain.like.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
     boolean existsByMemberIdAndReviewId(Long memberId, Long reviewId);
+
+    Optional<Like> findByMemberIdAndReviewId(Long membersId, Long reviewId);
 
 }
