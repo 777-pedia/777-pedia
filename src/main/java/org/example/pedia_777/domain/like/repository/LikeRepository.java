294c2fd55978349f019c8894ab26424a8c542476
soo59599 <sooh59599@gmail.com>
2025-09-26 20:52:59 +0900

    feat: 좋아요를 누른 리뷰 목록 조회 구현

--- src/main/java/org/example/pedia_777/domain/like/repository/LikeRepository.java
+++ src/main/java/org/example/pedia_777/domain/like/repository/LikeRepository.java
@@ -2,6 +2,9 @@ package org.example.pedia_777.domain.like.repository;
 
 import java.util.Optional;
 import org.example.pedia_777.domain.like.entity.Like;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
@@ -10,4 +13,6 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
 
     Optional<Like> findByMemberIdAndReviewId(Long memberId, Long reviewId);
 
+    @EntityGraph(attributePaths = {"review", "review.movie"})
+    Page<Like> findByMemberId(Long memberId, Pageable pageable);
 }
