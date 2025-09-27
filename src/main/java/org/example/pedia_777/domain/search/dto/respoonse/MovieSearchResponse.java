b3a25e1bc0d9f1261e6e9f60fa7065ce013bcc82
seonrizee <seonrizee@gmail.com>
2025-09-28 03:26:00 +0900

    feat: MovieSearchResponse에서 MovieSearchProjection을 사용하도록 변경

--- src/main/java/org/example/pedia_777/domain/search/dto/respoonse/MovieSearchResponse.java
+++ src/main/java/org/example/pedia_777/domain/search/dto/respoonse/MovieSearchResponse.java
@@ -2,31 +2,28 @@ package org.example.pedia_777.domain.search.dto.respoonse;
 
 import static org.example.pedia_777.common.util.StringHelper.splitSafely;
 
-import org.example.pedia_777.domain.movie.entity.Movie;
-
 public record MovieSearchResponse(
         Long id,
         String title,
         String director,
         String[] actors,
         String posterUrl,
-        double avgRating,
-        long reviewCount,
-        long favoriteCount
+        Double avgRating,
+        Long reviewCount,
+        Long favoriteCount
 ) {
 
 
-    public static MovieSearchResponse of(Movie movie, double avgScore,
-                                         long reviewCount, long likeCount) {
+    public static MovieSearchResponse from(MovieSearchProjection projectionDto) {
         return new MovieSearchResponse(
-                movie.getId(),
-                movie.getTitle(),
-                movie.getDirector(),
-                splitSafely(movie.getActors()),
-                movie.getPosterUrl(),
-                avgScore,
-                reviewCount,
-                likeCount
+                projectionDto.id(),
+                projectionDto.title(),
+                projectionDto.director(),
+                splitSafely(projectionDto.actors()),
+                projectionDto.posterUrl(),
+                Math.round(projectionDto.avgRating() * 100) / 100.0, // 소수점 둘째 자리까지 반올림
+                projectionDto.reviewCount(),
+                projectionDto.favoriteCount()
         );
     }
 }
