2ce6754a8cb0e978191f5eca4e7074e5fc40e0bc
seonrizee <seonrizee@gmail.com>
2025-09-28 03:29:50 +0900

    feat: 영화 검색 기능을 위한 searchMovies 메서드 추가

--- src/main/java/org/example/pedia_777/domain/movie/service/MovieServiceApi.java
+++ src/main/java/org/example/pedia_777/domain/movie/service/MovieServiceApi.java
@@ -1,8 +1,13 @@
 package org.example.pedia_777.domain.movie.service;
 
 import org.example.pedia_777.domain.movie.entity.Movie;
+import org.example.pedia_777.domain.search.dto.respoonse.MovieSearchResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface MovieServiceApi {
 
     Movie findMovieById(Long movieId);
+
+    Page<MovieSearchResponse> searchMovies(String keyword, Pageable pageable);
 }
