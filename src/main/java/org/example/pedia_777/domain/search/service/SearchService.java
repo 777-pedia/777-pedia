1638adf0fd92468b0b72558fd21d92708bb9fcd9
seonrizee <seonrizee@gmail.com>
2025-09-28 03:30:11 +0900

    feat: 영화 검색을 위한 SearchController 및 SearchService 추가

---
+++ src/main/java/org/example/pedia_777/domain/search/service/SearchService.java
@@ -0,0 +1,20 @@
+package org.example.pedia_777.domain.search.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.pedia_777.common.dto.PageResponse;
+import org.example.pedia_777.domain.movie.service.MovieService;
+import org.example.pedia_777.domain.search.dto.respoonse.MovieSearchResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SearchService {
+
+    private final MovieService movieService;
+
+    public PageResponse<MovieSearchResponse> searchMovies(String keyword, Pageable pageable) {
+
+        return PageResponse.from(movieService.searchMovies(keyword, pageable));
+    }
+}
