34ec20b6951e6cd1dbfbc837e8fb3cf67f8fcc08
seonrizee <seonrizee@gmail.com>
2025-09-29 16:51:20 +0900

    feat: 인기 검색어 조회 API 엔드포인트 추가

--- src/main/java/org/example/pedia_777/domain/search/controller/SearchController.java
+++ src/main/java/org/example/pedia_777/domain/search/controller/SearchController.java
@@ -1,11 +1,14 @@
 package org.example.pedia_777.domain.search.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.pedia_777.common.code.CommonSuccessCode;
 import org.example.pedia_777.common.dto.GlobalApiResponse;
 import org.example.pedia_777.common.dto.PageResponse;
 import org.example.pedia_777.common.util.ResponseHelper;
 import org.example.pedia_777.domain.search.dto.response.MovieSearchResponse;
+import org.example.pedia_777.domain.search.dto.response.PopularKeywordResponse;
+import org.example.pedia_777.domain.search.service.PopularSearchService;
 import org.example.pedia_777.domain.search.service.SearchService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SearchController {
 
     private final SearchService searchService;
+    private final PopularSearchService popularSearchService;
 
     @GetMapping("/api/v1/search")
     public ResponseEntity<GlobalApiResponse<PageResponse<MovieSearchResponse>>> searchMovies(
@@ -36,5 +40,13 @@ public class SearchController {
 
         return ResponseHelper.success(CommonSuccessCode.REQUEST_SUCCESS,
                 searchService.searchMoviesWithLocalCache(keyword, pageable));
+    }
+
+    @GetMapping("/api/v1/search/popular")
+    public ResponseEntity<GlobalApiResponse<List<PopularKeywordResponse>>> getPopularKeywords() {
+
+        // TODO 현재는 UI 기획대로 바로 이전 시간대 데이터만 조회하도록 구현, 시간대 선택 가능하도록 RequestParam 적용 가능
+        return ResponseHelper.success(CommonSuccessCode.REQUEST_SUCCESS,
+                popularSearchService.getPopularKeywordsOfPreviousHour());
     }
 }
