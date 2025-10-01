49247d8eef1d0f54953e4614d7f5cb6bea5b18e0
seonrizee <seonrizee@gmail.com>
2025-10-01 10:12:40 +0900

    feat: 트랜잭션 충돌 방지 설정을 위한 RedisConfig 클래스 추가 및 RedisTemplate 설정

---
+++ src/main/java/org/example/pedia_777/common/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package org.example.pedia_777.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // Redisson과의 트랜잭션 충돌로 인해 트랜잭션 관리에서 제외
+        redisTemplate.setEnableTransactionSupport(false);
+
+        return redisTemplate;
+    }
+}
