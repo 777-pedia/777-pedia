a506cdaca05b29571bd14cca8cf9920cfc9bec4d
seonrizee <seonrizee@gmail.com>
2025-10-02 05:39:33 +0900

    fix: 캐시 타입의 만료 시간을 조정

--- src/main/java/org/example/pedia_777/common/config/CacheType.java
+++ src/main/java/org/example/pedia_777/common/config/CacheType.java
@@ -7,12 +7,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CacheType {
-
+    
     MOVIE_SEARCH_CURRENT_POPULAR("movieSearchCurrentPopular", Duration.ofMinutes(5)), // 5분
-    MOVIE_SEARCH_PREV_POPULAR("movieSearchPrevPopular", Duration.ofMinutes(120)), // 2시간
-    LIST_POPULAR_KEYWORDS("popularKeywords", Duration.ofHours(26)), // 26시간
-    LIST_DAILY_TOP_10_MOVIES("dailyTop10Movies", Duration.ofHours(26)), // 26시간
-    LIST_WEEKLY_TOP_10_MOVIES("weeklyTop10Movies", Duration.ofDays(8)), // 8일
+    MOVIE_SEARCH_PREV_POPULAR("movieSearchPrevPopular", Duration.ofHours(2)), // 2시간
+    LIST_POPULAR_KEYWORDS("popularKeywords", Duration.ofHours(1)), // 1시간
+    LIST_DAILY_TOP_10_MOVIES("dailyTop10Movies", Duration.ofHours(24)), // 24시간
+    LIST_WEEKLY_TOP_10_MOVIES("weeklyTop10Movies", Duration.ofDays(7)), // 7일
     MOVIE_DETAILS_TOP_10("movieDetailsTop10", Duration.ofDays(8)); // 8일
 
     public static final String NAME_MOVIE_SEARCH_CURRENT_POPULAR = "movieSearchCurrentPopular";
